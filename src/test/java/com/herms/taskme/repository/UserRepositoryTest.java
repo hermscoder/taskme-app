aa5390448b96c73404c40fb7bd5a26fc516c4cb5
emerson.junior <emerson.junior@sankhya.com.br>
2020-08-06 10:49:31 -0300

    adding test coverage to userRepository

--- src/test/java/com/herms/taskme/repository/UserRepositoryTest.java
+++ src/test/java/com/herms/taskme/repository/UserRepositoryTest.java
@@ -1,20 +1,15 @@
 package com.herms.taskme.repository;
 
-import javax.validation.ConstraintViolation;
+
 import javax.validation.ConstraintViolationException;
 
-import org.assertj.core.api.AssertDelegateTarget;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.TransactionSystemException;
 
 import com.herms.taskme.model.User;
-
-import net.bytebuddy.asm.Advice.Thrown;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
