601729e8b6c0284fa6477ce50afa0effeb0276a0
Emerson Ribeiro <emersonrjr03@gmail.com>
2021-11-12 21:25:30 +0000

    fixing healthcheck endpoint

--- src/main/java/com/herms/taskme/TaskmeApplication.java
+++ src/main/java/com/herms/taskme/TaskmeApplication.java
@@ -1,21 +1,8 @@
 package com.herms.taskme;
 
-
-import com.herms.taskme.enums.FrequencyEnum;
-import com.herms.taskme.util.DateUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.Scanner;
 
 @SpringBootApplication
 public class TaskmeApplication {
